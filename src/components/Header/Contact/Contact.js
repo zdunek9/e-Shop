aa4b7538899c88d832c5c8c61fc459169aa9f138
Michal <m.zdunczyk2@gmail.com>
2022-06-14 22:14:46 +0200

    updated

--- src/components/Header/Contact/Contact.js
+++ src/components/Header/Contact/Contact.js
@@ -1,9 +1,29 @@
 import classes from "./Contact.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import twitter from "../../Img/icons/twitter.png";
+import facebook from "../../Img/icons/facebook.png";
+import github from "../../Img/icons/github.png";
+import linkedin from "../../Img/icons/linkedin.png";
 const Contact = () => {
   return (
     <section className={classes.wrapperContact} id="Contact">
-      sdsdsdsd
+      <div className={classes.bottomWrapperContact}>
+        <h3>Get In Touch</h3>
+        <a href="#">
+          <img src={twitter} />
+        </a>
+        <a href="#">
+          <img src={facebook} />
+        </a>
+        <a href="https://github.com/zdunek9" target="_blank">
+          <img src={github} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/micha%C5%82-zdu%C5%84czyk-a12754162"
+          target="_blank"
+        >
+          <img src={linkedin} />
+        </a>
+      </div>
     </section>
   );
 };
